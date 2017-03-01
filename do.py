5fac9da15682101842002384fc21e28d079b9c36
zkyyo <zkyyo@outlook.com>
2017-03-01 21:16:00 +0800

    add search function

--- do.py
+++ do.py
@@ -2,24 +2,41 @@ import os
 import time
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import quote
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 s = requests.Session()
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+driver = webdriver.PhantomJS(executable_path='/home/xu/a-project/python/dog/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
 
-def get_questions(s):
+def get_questions(url, s, driver):
     global bag
-    r = s.get("https://www.zhihu.com/search?type=content&q=%E8%AF%AD%E6%96%87", headers=headers)
-    bs0bj = BeautifulSoup(r.text, "lxml")
+    global link_bag
+#   r = s.get("https://www.zhihu.com/search?type=content&q=%E8%AF%AD%E6%96%87", headers=headers)
+#   bs0bj = BeautifulSoup(r.text, "lxml")
+
+    driver = webdriver.PhantomJS(executable_path='/home/xu/a-project/python/dog/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
+    driver.get(url)
+
+    # 4 times default
+    for i in range(2):
+        print("[{}]waiting..." .format(i))
+        page_button = driver.find_element(By.CLASS_NAME, "zu-button-more")
+        page_button.click()
+        time.sleep(1.5)
+
+    bs0bj = BeautifulSoup(driver.page_source, "lxml")
 
     topics = bs0bj.findAll("li", {"class": "item clearfix"})
     for topic in topics:
         title = topic.find("div", {"class": "title"})
         vote = topic.find("a", {"class": "zm-item-vote-count"})
         link = topic.find("div", {"class": "title"}).find("a")
-        bag.append([title.get_text(), vote.get_text(), link["href"]])
+        if link["href"] not in link_bag:
+            link_bag.append(link["href"])
+            bag.append([title.get_text(), vote.get_text(), link["href"]])
 
 def show_questions():
     global bah
@@ -27,6 +44,8 @@ def show_questions():
     for topic in bag:
         print(index, topic)
         index += 1
+        if index > 30:
+            break
 
 def read_answer_A(url, s, headers):
     r = s.get(url, headers=headers)
@@ -76,13 +95,24 @@ def read_answer_B(url, s, headers):
 
 
 bag = []
-os.system('clear')
-get_questions(s)
-show_questions()
+link_bag = []
 while True:
     action = input("what next? ")
     action = action.split(" ")
-    if action[0] == "back":
+    if action[0] == "search":
+        key_words = action[1:]
+        sentence = ''
+        for key_word in key_words:
+            sentence += key_word
+            sentence += ' '
+        url = "https://www.zhihu.com/search?type=content&q=" + quote(sentence)
+        os.system('clear')
+        get_questions(url, s, driver)
+        os.system('clear')
+        show_questions()
+
+
+    elif action[0] == "back":
         os.system('clear')
         show_questions()
     elif action[0] == "go":
@@ -96,8 +126,6 @@ while True:
     elif action[0] == "quit":
         print("see you, my friend.")
         break
-
-
 
 
 
