ad002517363bf6e61b705038a872f4ef94483632
zkyyo <zkyyo@outlook.com>
2017-03-03 18:36:08 +0800

    add the choice of get more questions

--- sample.py
+++ sample.py
@@ -5,27 +5,28 @@ from bs4 import BeautifulSoup
 from urllib.parse import quote
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 s = requests.Session()
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
-driver = webdriver.PhantomJS(executable_path='/home/xu/a-project/python/dog/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
 
-def get_questions(url, s, driver):
+def get_questions(url):
     global one_topic_bag
     global link_bag
-#   r = s.get("https://www.zhihu.com/search?type=content&q=%E8%AF%AD%E6%96%87", headers=headers)
-#   bs0bj = BeautifulSoup(r.text, "lxml")
 
     driver = webdriver.PhantomJS(executable_path='/home/xu/a-project/python/dog/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
     driver.get(url)
 
-    # 4 times default
+    # 2 times default
     for i in range(2):
         print("[{}]waiting..." .format(i))
-        page_button = driver.find_element(By.CLASS_NAME, "zu-button-more")
-        page_button.click()
-        time.sleep(1.5)
+        try:
+            page_button = driver.find_element(By.CLASS_NAME, "zu-button-more")
+            page_button.click()
+            time.sleep(1.5)
+        except NoSuchElementException:
+            break
 
     bs0bj = BeautifulSoup(driver.page_source, "lxml")
 
@@ -38,14 +39,44 @@ def get_questions(url, s, driver):
             link_bag.append(link["href"])
             one_topic_bag.append([title.get_text(), vote.get_text(), link["href"]])
 
+    return driver
+
+def get_more_questions(url, driver):
+    global one_topic_bag
+    global link_bag
+    global is_first_search
+
+    for i in range(3):
+        print("[{}]waiting..." .format(i))
+        try:
+            page_button = driver.find_element(By.CLASS_NAME, "zu-button-more")
+            page_button.click()
+            time.sleep(1.5)
+        except NoSuchElementException:
+            break
+
+    bs0bj = BeautifulSoup(driver.page_source, "lxml")
+
+    topics = bs0bj.findAll("li", {"class": "item clearfix"})
+    for topic in topics:
+        title = topic.find("div", {"class": "title"})
+        vote = topic.find("a", {"class": "zm-item-vote-count"})
+        link = topic.find("div", {"class": "title"}).find("a")
+        if link["href"] not in link_bag:
+            link_bag.append(link["href"])
+            one_topic_bag.append([title.get_text(), vote.get_text(), link["href"]])
+
+    return driver
+
+
+
 def show_questions():
     global one_topic_bag
+    global first_search
     index = 0
     for topic in one_topic_bag:
         print("[{:d}] {} (vote {}) " .format(index, topic[0], topic[1]))
         index += 1
-        if index > 30:
-            break
 
 def read_answer_A(url, s, headers):
     r = s.get(url, headers=headers)
@@ -62,12 +93,15 @@ def read_answer_A(url, s, headers):
     else:
         print("<---plane A--->")
 
+    i = 0
     try: 
         print(">" * 150)
         print(question_title.get_text().strip())
         print(question_detail.get_text().strip())
         print(">" * 150)
         for answer in answers:
+            print(i)
+            i += 1
             print(answer.get_text(separator='\n'))
             print(">" * 100)
     except AttributeError:
@@ -102,10 +136,12 @@ def show_search_history():
 begin = time.time()
 search_history_bag = []
 link_bag = []
+is_first_search = True
 os.system('clear')
 while True:
     action = input("\nwhat next? ")
     action = action.split(" ")
+    # search the questions
     if action[0] == "search":
         one_topic_bag = []
         key_words = action[1:]
@@ -115,13 +151,20 @@ while True:
             sentence += key_word
             sentence += ' '
         url = "https://www.zhihu.com/search?type=content&q=" + quote(sentence)
-        os.system('clear')
-        get_questions(url, s, driver)
+        first_search_driver = get_questions(url)
+        is_first_search = False
         os.system('clear')
         show_questions()
+    # search more questions in the basic of the first search
+    elif action[0] == "more" and is_first_search == False:
+        get_more_questions(url, first_search_driver)
+        os.system('clear')
+        show_questions()
+    # back to the question page
     elif action[0] == "back":
         os.system('clear')
         show_questions()
+    # get into one of the question
     elif action[0] == "go":
         try:
             index_enter = int(action[1])
@@ -130,6 +173,7 @@ while True:
             read_answer_A(url, s, headers)
         except:
             continue
+    # quit the program
     elif action[0] == "quit":
         end = time.time()
         seconds = end - begin
@@ -140,14 +184,17 @@ while True:
             print("* spend {:.2f} minutes" .format(minutes))
         print("* see you, my friend.")
         break
+    # get some help information
     elif action[0] == "help":
         print("* search [key words]      <----support multiple key words")
         print("* go [question index]")
         print("* back                    <----back to questions page")
         print("* help                    <----show this page")
         print("                          <----made by zkyyo")
+    # show the history of the key words
     elif action[0] == "history":
         show_search_history()
+    # author
     elif action[0] == "zkyyo":
         print("* it's me")
 
